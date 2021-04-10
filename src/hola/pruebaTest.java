4393448e1aa4ea579bdff7843b30c0812d451a92
cristian <cquipildor4@gmail.com>
2021-04-10 18:58:24 -0300

    cuarto commit

---
+++ src/hola/pruebaTest.java
@@ -0,0 +1,14 @@
+package hola;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class pruebaTest {
+    @Test
+    public void holaCrack(){
+        String hola="cristian";
+
+        Assert.assertEquals("cristian",hola);
+
+    }
+}
