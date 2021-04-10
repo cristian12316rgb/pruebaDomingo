e3164cbce4a8df382a56f0376c6bd8a6e0d4d9f6
cristian <cquipildor4@gmail.com>
2021-04-10 18:38:40 -0300

    tercer commit

--- src/hola/prueba.java
+++ src/hola/prueba.java
@@ -1,14 +1,4 @@
 package hola;
 
 public class prueba {
-    private String color;
-    private String nombre;
-    prueba(String color, String nombre){
-        this.color=color;
-        this.nombre=nombre;
-    }
-
-    
-
-
 }
